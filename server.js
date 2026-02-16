74f753998c9f5a3b79d40e0299993ba1a82ddd5a
Mazen Ibra <mazenibra89@gmail.com>
2026-02-16 09:51:16 +0700

    delete

--- server.js
+++ server.js
@@ -18,9 +18,9 @@ app.use(cors({
 
 app.use(express.json());
 
-app.use('/', (req, res) => {
-  res.send('Selamat datang di API POS!');
-});
+// app.use('/', (req, res) => {
+//   res.send('Selamat datang di API POS!');
+// });
 
 app.use('/api/auth', authRoutes);
 
